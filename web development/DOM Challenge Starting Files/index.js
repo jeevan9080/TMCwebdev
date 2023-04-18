23a42ce39ce94c82f23e9e0f1edf6501e055596b
jeevan9080 <r.jeevan100@gmail.com>
2023-04-18 09:33:52 +0530

    18.4.23

---
+++ web development/DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelector("button").style.backgroundColor="yellow";
+
+document.querySelector("h1").classList.add("huge");
+
+document.querySelector("a").setAttribute("href","https://www.bing.com");
